7ab014c767a49102d5aebe62ce7abaaff9a5129e
Ippili Sidhartha Kumar <iskumar789@gmail.com>
2019-09-09 09:28:19 +0530

    update group data on changing individual data
    - Update preference data in data_groups from data_individuals
    - modify allocated field in data_groups on changing individual allocation

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(function () {
     var headings = [];
     var uploaded_individuals = false;
     var uploaded_groups = false;
-
+    var associated_individuals_groups = [];
     document.getElementById('file-preferences').onchange = function () {
         var file = this.files[0];
         var reader = new FileReader();
@@ -62,9 +62,26 @@ $(function () {
             }
             document.getElementById("file-slots").style.display = "none";
             uploaded_groups = true;
-            /*
-            Update preference data in data_groups from data_individuals here
-            */
+
+            $.each(data_groups, function (index, group) {
+                associated_individuals = [];
+                $.each(data_individuals, function (index, individual) {
+                    pos = $.inArray(group["Unique-id"], individual.choices.split(";"));
+                    if (pos != -1) {
+                        x = new Object();
+                        x.pos = pos + 1;
+                        x.id = individual["Unique-id"];
+                        x.name = individual["Full-Name"];
+                        associated_individuals.push(x);
+                    }
+                });
+                associated_individuals.sort(function (a, b) {
+                    return a.pos - b.pos;
+                });
+                individuals = associated_individuals.map(a => a.id);
+                (data_groups[group["SrNum"] - 1]).choices = individuals.join(";");
+                associated_individuals_groups[group["Unique-id"]] = associated_individuals;
+            });
             $("#jsgrid-slots").jsGrid({
                 height: "100%",
                 width: "100%",
@@ -83,7 +100,7 @@ $(function () {
                     var $info = $("<div>").addClass("client-info")
                         .append($("<p>").append($("<strong>").text(grp["Full-Name"])));
 
-                    return $("<tr>").append($("<td>").append(grp["Unique-id"])).append($("<td>").append($info)).append($("<td>").append(grp.capacity));
+                    return $("<tr>").append($("<td>").append(grp["Unique-id"])).append($("<td>").append($info)).append($("<td>").append((grp.allocated == null ? 0 : (grp.allocated.split(";").length)) + " / " + grp.capacity));
                 },
                 fields: [
                     { title: "Course code" }, { title: "Course name" }, { title: "Requirement" }]
@@ -131,8 +148,34 @@ $(function () {
             });
         }
 
-        formSubmitHandler = function () {
-            saveClient(client);
+        formSubmitHandlerPref = function () {
+            var allocated = [];
+            $("#preference input").each(function (index, value) {
+                if (value.checked) {
+                    allocated.push(value.id);
+                    group = data_groups.find(obj => { return obj["Unique-id"] == value.id });
+                    y = group.allocated == null ? [] : group.allocated.split(";");
+                    y.push(client["Unique-id"]);
+                    group.allocated = y.join(";");
+                    data_groups[group["SrNum"] - 1] = group;
+                }
+                else {
+                    group = data_groups.find(obj => { return obj["Unique-id"] == value.id });
+                    y = group.allocated == null ? [] : group.allocated.split(";");
+                    index = y.indexOf(client["Unique-id"]);
+                    if (index > -1) {
+                        y.splice(index, 1);
+                        group.allocated = y.length > 0 ? y.join(";") : null;
+                        data_groups[group["SrNum"] - 1] = group;
+                    }
+                }
+            });
+            client["allocated"] = allocated.length > 0 ? allocated.join(";") : null;
+            data_individuals[client.SrNum - 1] = client;
+            $("#jsgrid-preference").jsGrid("refresh");
+            $("#jsgrid-slots").jsGrid("refresh");
+
+            $("#detailsDialogPref").dialog("close");
         };
 
         $("#detailsDialogPref").dialog("option", "title", dialogType + " Client")
@@ -140,32 +183,44 @@ $(function () {
     };
 
     var showGroup = function (dialogType, client) {
-        $("#nameSlot").val(client["Full-Name"]);
-        formSubmitHandler = function () {
-            saveClient(client, dialogType === "Add");
+        $("#nameSlot").html(client["Full-Name"]);
+        $("#slot div").remove();
+
+
+        if (associated_individuals_groups[client["Unique-id"]] != []) {
+            $.each(associated_individuals_groups[client["Unique-id"]], function (index, individual) {
+                $("#slot").append($("<div class=form-group>").append($('<label>', {
+                    for: individual.id
+                }).text(individual.name)).append($('<input>', {
+                    type: 'checkbox',
+                    id: individual.id,
+                    name: individual.id
+                }).prop("checked", false)));
+            });
+        }
+
+        formSubmitHandlerSlot = function () {
+            /* Update data_individuals also */
+
         };
+
         $("#detailsDialogSlot").dialog("option", "title", dialogType + " Client")
             .dialog("open");
     };
 
     $("#detailsFormPref").validate({
         submitHandler: function () {
-            formSubmitHandler();
+            formSubmitHandlerPref();
         }
     });
 
-    var formSubmitHandler = $.noop;
-
-    var saveClient = function (client) {
-        var allocated = [];
-        $("#preference input").each(function (index, value) {
-            if (value.checked) allocated.push(value.id);
-        });
-        client["allocated"] = allocated.join(";");
-        data_individuals[client.SrNum - 1] = client;
-        $("#jsgrid-preference").jsGrid("refresh");
-        $("#detailsDialogPref").dialog("close");
-    };
+    $("#detailsFormSlot").validate({
+        submitHandler: function () {
+            formSubmitHandlerSlot();
+        }
+    });
+    var formSubmitHandlerPref = $.noop;
+    var formSubmitHandlerSlot = $.noop;
 
     $("#saveFiles").click(function () {
         if (!uploaded_individuals) alert("Load both files first !");
